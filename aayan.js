b7ac51abdd5a295d4c4bb45812e226ed6acb4c1e
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-03-01 12:43:16 +0500

    Update and rename ayan.js to aayan.js

--- aayan.js
+++ aayan.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
       to: "ta0134750@gmail.com,ayanali1288383@gmail.com",
-      subject: "ayan",
+      subject: "aayan",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
